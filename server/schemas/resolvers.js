7fba1addce6821c9caca9f97272b8713e6dfcba0
MJ_Shelton <mjshelton12@comcast.net>
2022-11-07 09:08:39 -0800

    Updated resolvers, typeDefs, and authentification system

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,46 @@
-const { User } = require('../models');
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
+const { signToken } = require("../utils/auth");
+const { sign } = require("jsonwebtoken");
 const resolvers = {
-    Query: {
-        user: async () => {
-            return User.findOne({_id: userId})
-        }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+        return userData;
+      }
+      throw new AuthenticationError('need to be logged In')
     },
+  },
 
-   Mutation: {
-        addUser: async (parent, { body }) => {
-            return User.create({ body });
-        },
-        signToken: async (parent, { userId, token }) => {
-            return User.findOneAndUpdate(
-                {_id: userId },
-                { new: true, runValidators: true}
-            )
-        }
-    }
-}
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
 
- 
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email: email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect email");
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError("Incorrect Password");
+      }
+
+      const token = sign(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,15 +6,30 @@ const typeDefs = gql`
     username: String
     email: String
     bookCount: Int
-    savedBooks: [Books]
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
   }
 
   type Query {
-    me(userId: ID!): User
+    me: User
   }
 
   type Mutation {
-    login
+    login(): Auth
+    
   }
 `;
 
